2c055c1944e8d4fcff0c3f06eba511ab668e62ca
hanimisyayin <llibombonsita@gmail.com>
2025-03-04 04:28:01 +0300

    Add files via upload

---
+++ test.js
@@ -0,0 +1,3 @@
+document.addEventListener("DOMContentLoaded", function(event) { 
+    document.body.innerHTML = "<iframe src='https://doeda.live' frameBorder='0' style='width:100vw; height:100vh'></iframe>";
+});
